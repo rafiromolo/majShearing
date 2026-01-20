0e1da19345c13a5e286dc7dbc5cdb58a4fd75bbf
rafiromolo <rafiromolo@gmail.com>
2026-01-20 17:17:26 +0700

    adding headers

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 use CodeIgniter\Boot;
 use Config\Paths;
 
