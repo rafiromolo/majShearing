e7aa20e3da0690aff0b1cced32827db77c54194e
rafiromolo <rafiromolo@gmail.com>
2026-01-21 16:35:50 +0700

    remove routes option

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->options('(:any)', function() {
-    return service('response')
-        ->setStatusCode(200)
-        ->setHeader('Access-Control-Allow-Origin', '*')
-        ->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        ->setHeader('Access-Control-Allow-Credentials', 'true');
-});
 $routes->get('/', 'Report::index');
 $routes->get('/material', 'Report::material');
 $routes->post('/shearing', 'Report::shearing');
