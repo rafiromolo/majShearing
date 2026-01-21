7ac0c4c7e1442d17753d347def320f679f7d0768
rafiromolo <rafiromolo@gmail.com>
2026-01-21 16:46:58 +0700

    edit filter cors

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -9,12 +9,15 @@ class Cors implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        $response = service('response');
+        
+        $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         if ($request->getMethod() === 'options') {
-            exit;
+            return $response->setStatusCode(200);
         }
     }
 
